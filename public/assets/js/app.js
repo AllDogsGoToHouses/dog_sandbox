9aa85443313da9367eb6fb28007cc86e2323a6d6
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-06-11 03:28:44 -0700

    Clean up code. Finished Shelter/Adopter Page/ Basic Layout for Results & Favorites. Added javascript for adding favorites into cards

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,24 +1,18 @@
+// Materialize JavaScript
 $(document).ready(function(){
     $('.modal').modal()
     $('.sidenav').sidenav()
     $('.tooltipped').tooltip();
-    
-   
   })
 
 // Add Favorite Cards
   $("#add-fave").on("click", function(){
-    addFave()
-})
-    function addFave(){
-        $("#faves-holder").append(`<div class="col s3 m5">
-            <div class="card">
-                <div class="card-image">
-                    <img src="https://placeimg.com/640/480/animals">
-                    </div>
-                <div class="card-action">
-                    <p> Favorite's Name</p>
-                </div>
-            `
-    )
-} 
+      imageURL = "https://placeimg.com/640/480/animals"
+      name = "Favorite's Name"
+    addFave(imageURL,name)
+
+    })
+
+    function addFave(imageURL, name){
+        $("#faves-holder").append('<div class="col s3 m3"> <div class="card"><div class="card-image"><img src="' + imageURL + '"></div><div class="card-action"><p>'+ name + '</p></div>')
+    } 
