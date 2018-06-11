5e852bddc57b4114920aefe4985acd363896e7d9
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-06-11 02:02:18 -0700

    Cleaned up shelter.html

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
     $('.modal').modal()
-    $('.sidenav').sidenav();
+    $('.sidenav').sidenav()
+    $('.tooltipped').tooltip();
+    
    
   })
-        
+
+// Add Favorite Cards
+  $("#add-fave").on("click", function(){
+    addFave()
+})
+    function addFave(){
+        $("#faves-holder").append(`<div class="col s3 m5">
+            <div class="card">
+                <div class="card-image">
+                    <img src="https://placeimg.com/640/480/animals">
+                    </div>
+                <div class="card-action">
+                    <p> Favorite's Name</p>
+                </div>
+            `
+    )
+} 
