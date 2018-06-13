341335b1b1b3a3c37238d6eab4b836749c837504
wodjafink <wodjafink@gmail.com>
2018-06-13 10:47:07 -0700

    Retabbed the models and added fields to specifically not have fields for createdAt and updatedAt

--- models/adopter.js
+++ models/adopter.js
@@ -1,15 +1,17 @@
 module.exports = function(sequelize, DataTypes) {
     var Adopter = sequelize.define("Adopter", {
- adopter_name: {
-    type: DataTypes.STRING
-  },
-  adopter_email: {
-    type: DataTypes.STRING
-  },
-  adopter_favorites: {
-    type: DataTypes.STRING
-  },
-},{
+       adopter_name: {
+          type: DataTypes.STRING
+        },
+        adopter_email: {
+          type: DataTypes.STRING
+        },
+        adopter_favorites: {
+          type: DataTypes.STRING
+        },
+        createdAt: false,
+        updatedAt: false,
+      },{
       classMethods: {
         associate: function(models) {
           // associations can be defined here
--- models/shelter.js
+++ models/shelter.js
@@ -1,18 +1,20 @@
 module.exports = function(sequelize, DataTypes) {
     var Shelter = sequelize.define("Shelter", {
- shelter_name: {
-    type: DataTypes.STRING
-  },
-  shelter_manager: {
-    type: DataTypes.STRING
-  },
-  shelter_number: {
-    type: DataTypes.STRING
-  },
-  shelter_email: {
-    type: DataTypes.STRING
-  },
-},{
+      shelter_name: {
+        type: DataTypes.STRING
+      },
+      shelter_manager: {
+        type: DataTypes.STRING
+      },
+      shelter_number: {
+        type: DataTypes.STRING
+      },
+      shelter_email: {
+        type: DataTypes.STRING
+      },
+      createdAt: false,
+      updatedAt: false,
+      },{
       classMethods: {
         associate: function(models) {
           // associations can be defined here
