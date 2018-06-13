3bbd7b2240bf11c9dc95af8dc90fe7364e8235a6
wodjafink <wodjafink@gmail.com>
2018-06-12 20:23:22 -0700

    Now merge was added in

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,7 @@
 USE dogs_db;
 
-<<<<<<< HEAD:seeds.sql
-CREATE TABLE dogs (
-  dog_id INT NOT NULL AUTO_INCREMENT,
-  dog_name VARCHAR(100) NOT NULL,
-  dog_image_url VARCHAR(200) NOT NULL,
-  dog_size VARCHAR(10) NOT NULL,
-  age INT NOT NULL,
-  activity_level VARCHAR(20) NOT NULL,
-  gender VARCHAR(100),
-  breed VARCHAR(100),
-  likes VARCHAR(200),
-  dislikes VARCHAR(200),
-  shelter_id INT,
-  PRIMARY KEY (dog_id)
-);
-
-INSERT INTO dogs (dog_name, dog_image_url, dog_size, age, activity_level, gender, breed, likes, dislikes, shelter_id)
-VALUES ("Marley", "http://www.toptenz.net/wp-content/uploads/2013/10/rasta-dog.jpg", "Small", 15, "Active", "Male/Neutered", "Pitbull" "Weed, Chillin, Small Birds", "Loud Noises, Cops", 1), 
-=======
 INSERT INTO dogs (dog_name, dog_image_url, dog_size, age, activity_level, gender, breed, likes, dislikes, shelter_id)
 VALUES ("Marley", "http://www.toptenz.net/wp-content/uploads/2013/10/rasta-dog.jpg", "Small", 15, "Active", "Male/Neutered", "Pitbull", "Weed, Chillin, Small Birds", "Loud Noises, Cops", 1), 
->>>>>>> 85c39e5bb338295c5bc0f27785d9a26c40f6605a:db/seeds.sql
 
 ("Google", "https://i.imgflip.com/1f0068.jpg", "Small", 13, "Playful", "Male", "Chihuahua", "Kids, Big Dogs", "Small Dogs, bugs, bug eyed things", 2),
 
