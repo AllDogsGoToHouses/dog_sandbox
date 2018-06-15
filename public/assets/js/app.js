d8d6bd71ce6845a06663e6a9c39cf595e75d0d5f
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-06-15 11:53:21 -0700

    Login Modal Edits

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     $('.tooltipped').tooltip()
     $('.carousel.carousel-slider').carousel({fullWidth: true});
   })
-var signUpHolder = $("#signup-holder")
+
 // Create Account
 $("#create-account").on("click", function(){
     signUpHolder.html("Make Account")
@@ -13,53 +13,7 @@ $("#create-account").on("click", function(){
 })
 
 
-// Opens Email Login
-$("#e-signup").on("click", function(){
-    signUpHolder.html(`
-      <div class="container">
-        <div class="row" style="display: inline-block;">
-          <form class="col s12" method="post">
-            <div class='row'>
-              <div class='col s12'>
-              </div>
-            </div>
 
-            <div class='row'>
-              <div class='input-field col s12'>
-                <input class='validate' type='email' name='email' id='email' />
-                <label for='email'>Enter your email</label>
-              </div>
-            </div>
-
-            <div class='row'>
-              <div class='input-field col s12'>
-                <input class='validate' type='password' name='password' id='password' />
-                <label for='password'>Enter your password</label>
-              </div>
-              <label style='float: right;'>
-								<a href='#!'><b>Forgot Password?</b></a>
-							</label>
-            </div>
-
-            <br />
-            <center>
-              <div class='row'>
-                <button type='submit' name='btn_login' class='col s12 btn btn-large waves-effect signup-btns'>Login</button>
-              </div>
-            </center>
-          </form>
-        </div>
-      </div>
-    </center>
-
-    <div class="section"></div>
-    <div class="section"></div>
-  </main>
-    
-    
-    
-    `)
-})
   // Sign Up Buttons
 $("#email").on("click", function(){
     signUpType = "email"
