679f1b240274d973db99d72ca8a62a9527c6885b
wodjafink <wodjafink@gmail.com>
2018-06-16 12:46:54 -0700

    Updated the html_routes to use handlebars rendering

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,29 +5,23 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads index.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+  app.get('/', function (req, res) {
+    res.render('index');
   });
 
-  app.get("/adopter", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/adopter.html"));
+  app.get('/adopter', function (req, res) {
+    res.render('adopter');
   });
 
-  app.get("/faves", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/faves.html"));
+  app.get('/shelter', function (req, res) {
+    res.render('shelter');
   });
 
-  app.get("/index", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+  app.get('/results', function (req, res) {
+    res.render('results');
   });
 
-  app.get("/results", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/results.html"));
+  app.get('/faves', function (req, res) {
+    res.render('faves');
   });
-
-  app.get("/shelter", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/shelter.html"));
-  });
-
-
 };
